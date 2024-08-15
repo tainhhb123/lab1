847594355b7382ad045c692ccbe2473b82fdf671
tainhhb123 <trantantai310803@gmail.com>
2024-08-15 16:11:23 +0700

    lab1

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I am rich',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('I am rich'),
+      ),
+      body: Center(
+        child: Image.asset('assets/images/playstore.png'),
+      ),
+    );
+  }
+}
